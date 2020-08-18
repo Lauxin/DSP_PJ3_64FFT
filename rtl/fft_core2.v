07cb658d1b5a0ecccc6fa04947ee8c6835793625
Lauxin <976144243@qq.com>
2020-08-19 02:44:39 +0800

    [RTL][UPDATE] a few modify in fft_core2

--- rtl/fft_core2.v
+++ rtl/fft_core2.v
@@ -49,17 +49,17 @@ module fft_core2(
   assign fft_dat2_re_i_w = fft_dat2_i[2*DATA_INP_WD -1 : DATA_INP_WD];
   assign fft_dat2_im_i_w = fft_dat2_i[DATA_INP_WD   -1 : 0          ];
 
-  assign fft_wn_re_i_w = fft_wn_i[2*`CFG_WN_WD -1 : `CFG_WN_WD ];
-  assign fft_wn_im_i_w = fft_wn_i[  `CFG_WN_WD -1 : 0          ];
+  assign fft_wn_re_i_w = fft_wn_i[2*`CFG_WN_WD -1 : `CFG_WN_WD];
+  assign fft_wn_im_i_w = fft_wn_i[  `CFG_WN_WD -1 : 0         ];
 
-  // TODO:
   // "*" and "<<" in verilog will expend to the output width
   always @(*) begin
-    fft_cal1_re_w = (fft_dat1_re_i_w << `DATA_FRA_WD) + (fft_dat2_re_i_w*fft_wn_re_i - fft_dat2_im_i_w*fft_wn_im_i); 
-    fft_cal1_im_w = (fft_dat1_im_i_w << `DATA_FRA_WD) + (fft_dat2_re_i_w*fft_wn_im_i + fft_dat2_im_i_w*fft_wn_re_i);
-    fft_cal2_re_w = (fft_dat1_re_i_w << `DATA_FRA_WD) - (fft_dat2_re_i_w*fft_wn_re_i - fft_dat2_im_i_w*fft_wn_im_i);
-    fft_cal2_im_w = (fft_dat1_im_i_w << `DATA_FRA_WD) - (fft_dat2_re_i_w*fft_wn_im_i + fft_dat2_im_i_w*fft_wn_re_i);
+    fft_cal1_re_w = (fft_dat1_re_i_w << `DATA_FRA_WD) + (fft_dat2_re_i_w*fft_wn_re_i_w - fft_dat2_im_i_w*fft_wn_im_i_w); 
+    fft_cal1_im_w = (fft_dat1_im_i_w << `DATA_FRA_WD) + (fft_dat2_re_i_w*fft_wn_im_i_w + fft_dat2_im_i_w*fft_wn_re_i_w);
+    fft_cal2_re_w = (fft_dat1_re_i_w << `DATA_FRA_WD) - (fft_dat2_re_i_w*fft_wn_re_i_w - fft_dat2_im_i_w*fft_wn_im_i_w);
+    fft_cal2_im_w = (fft_dat1_im_i_w << `DATA_FRA_WD) - (fft_dat2_re_i_w*fft_wn_im_i_w + fft_dat2_im_i_w*fft_wn_re_i_w);
   end
+  
   // fraction round / floor
   assign fft_dat1_re_o_w = (fft_cal1_re_w >> `DATA_FRA_WD) /*+ fft_cal_re1_out[`DATA_FRA_WD -1]*/;
   assign fft_dat1_im_o_w = (fft_cal1_im_w >> `DATA_FRA_WD) /*+ fft_cal_im1_out[`DATA_FRA_WD -1]*/;
