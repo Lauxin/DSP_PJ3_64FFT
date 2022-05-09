e4fb7a3a184751c2e3c9ebed06c9113ea30fec82
TrevorLiu <976144243@qq.com>
2022-05-09 23:06:15 +0800

    [RTL & SIM] 1.modify FFT_MEM 2.add sim for FFT_MEM, check PASS!

--- rtl/fft_mem.v
+++ rtl/fft_mem.v
@@ -11,29 +11,28 @@ module fft_mem (
   rst_n       ,
   clk         ,
 //--- 1x8 ---
-  dim_1x8_i   ,
+  dim_sel_i   ,
   adr_1x8_i   ,
   //rd
-  rd_val_1x8_i,
-  rd_val_1x8_o,
+  rd_vld_1x8_i,
+  rd_vld_1x8_o,
   rd_dat_1x8_o,
   //wr
-  wr_val_1x8_i,
+  wr_vld_1x8_i,
   wr_dat_1x8_i,
 //--- 1x1 ---
-  adr_x_1x1_i ,
-  adr_y_1x1_i ,
+  adr_1x1_i   ,
   //rd
-  rd_val_1x1_i,
-  rd_val_1x1_o,
+  rd_vld_1x1_i,
+  rd_vld_1x1_o,
   rd_dat_1x1_o,
   //wr
-  wr_val_1x1_i,
-  wr_dat_1x1_i,
+  wr_vld_1x1_i,
+  wr_dat_1x1_i
 );
 
 //*** PARAMETER ************
-  parameter DATA_WD = -1 ;
+  parameter DATA_WD = 10 ;
 
   localparam SIZE_MAT        = 8 ;
   localparam SIZE_MAT_WD     = 3 ;
@@ -41,129 +40,132 @@ module fft_mem (
   localparam SIZE_MAT_FUL_WD = SIZE_MAT_WD + SIZE_MAT_WD ;
 
 //*** INPUT/OUTPUT *********
-  input                               clk          ;
-  input                               rst_n        ;
+  input                               clk         ;
+  input                               rst_n       ;
 //--- 1x8 ---
-  input     [1                 -1 :0] dim_1x8_i    ;  //0: row; 1: col
-  input     [SIZE_MAT_WD       -1 :0] adr_1x8_i    ;
+  input     [1                 -1 :0] dim_sel_i   ;  //0: row; 1: col
+  input     [SIZE_MAT_WD       -1 :0] adr_1x8_i   ;
   //rd
-  input                               rd_val_1x8_i ;
-  output                              rd_val_1x8_o ;
-  output    [SIZE_MAT*DATA_WD  -1 :0] rd_dat_1x8_o ;
+  input                               rd_vld_1x8_i;
+  output                              rd_vld_1x8_o;
+  output    [SIZE_MAT*DATA_WD  -1 :0] rd_dat_1x8_o;
   //wr
-  input                               wr_val_1x8_i ;
-  input     [SIZE_MAT*DATA_WD  -1 :0] wr_dat_1x8_i ;
+  input                               wr_vld_1x8_i;
+  input     [SIZE_MAT*DATA_WD  -1 :0] wr_dat_1x8_i;
 //--- 1x1 ---
-  input     [SIZE_MAT_WD       -1 :0] adr_x_1x1_i  ;
-  input     [SIZE_MAT_WD       -1 :0] adr_y_1x1_i  ;
+  input     [SIZE_MAT_FUL_WD   -1 :0] adr_1x1_i   ;
   //rd
-  input                               rd_val_1x1_i ;
-  output                              rd_val_1x1_o ;
-  output    [DATA_WD           -1 :0] rd_dat_1x1_o ;
+  input                               rd_vld_1x1_i;
+  output                              rd_vld_1x1_o;
+  output    [DATA_WD           -1 :0] rd_dat_1x1_o;
   //wr
-  input                               wr_val_1x1_i ;
-  input     [DATA_WD           -1 :0] wr_dat_1x1_i ;
+  input                               wr_vld_1x1_i;
+  input     [DATA_WD           -1 :0] wr_dat_1x1_i;
 
 
 //*** WIRE/REG *************
-  wire      [SIZE_MAT_FUL_WD   -1 :0] adr_1x1_w    ;
-  reg                                 rd_val_1x1_r ;
-  reg                                 rd_val_1x8_r ;
-  reg       [DATA_WD           -1 :0] rd_dat_1x1_r ;
-  reg       [SIZE_MAT*DATA_WD  -1 :0] rd_dat_1x8_r ;
+  reg                                 rd_vld_1x1_r;
+  reg                                 rd_vld_1x8_r;
+  reg       [DATA_WD           -1 :0] rd_dat_1x1_r;
+  reg       [SIZE_MAT*DATA_WD  -1 :0] rd_dat_1x8_r;
   reg       [DATA_WD           -1 :0] mem_array[0:SIZE_MAT*SIZE_MAT-1] ;
 
+  integer                             i;
 
 //*** MAIN BODY ************
-  assign adr_1x1_w = (adr_y_1x1_i << 'd3) + adr_x_1x1_i ;
-  assign wr_val_i_w = wr_val_1x1_i || wr_val_1x8_i ;
-  assign rd_val_i_w = rd_val_1x1_i || rd_val_1x8_i ;
-
+//--- mem display ---
+//--- [0 : 7]
+//--- [8 :15]
+//--- [16:23]
+//--- [24:31]
+//--- [32:39]
+//--- [40:47]
+//--- [48:55]
+//--- [56:63]
   //wr
-  always @ (posedge clk) begin
-    if (wr_val_i_w) begin
-      if (wr_val_1x1_i) begin
-        mem_array[adr_1x1_w] <= wr_dat_1x1_i ;
+  always @ (posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      for (i=0; i<SIZE_MAT_FUL; i=i+1) begin
+        mem_array[i] <= 'hfff;
       end
-      else begin  //wr_val_1x8_i
-        if (dim_1x8_i) begin
-          mem_array[(adr_1x8_i << 'd3)      ] <= wr_dat_1x8_i[      DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd1] <= wr_dat_1x8_i['d2 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd2] <= wr_dat_1x8_i['d3 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd3] <= wr_dat_1x8_i['d4 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd4] <= wr_dat_1x8_i['d5 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd5] <= wr_dat_1x8_i['d6 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd6] <= wr_dat_1x8_i['d7 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[(adr_1x8_i << 'd3) + 'd7] <= wr_dat_1x8_i['d8 * DATA_WD-1 -: DATA_WD] ;
-        end
-        else begin
-          mem_array[               adr_1x8_i] <= wr_dat_1x8_i[      DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d1 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d2 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d2 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d3 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d3 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d4 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d4 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d5 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d5 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d6 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d6 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d7 * DATA_WD-1 -: DATA_WD] ;
-          mem_array[('d7 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d8 * DATA_WD-1 -: DATA_WD] ;
-        end
+    end
+    else if (wr_vld_1x1_i) begin
+      mem_array[adr_1x1_i] <= wr_dat_1x1_i ;
+    end
+    else if (wr_vld_1x8_i) begin
+      if (!dim_sel_i) begin  //row
+        mem_array[(adr_1x8_i << 'd3) + 'd0] <= wr_dat_1x8_i[      DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd1] <= wr_dat_1x8_i['d2 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd2] <= wr_dat_1x8_i['d3 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd3] <= wr_dat_1x8_i['d4 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd4] <= wr_dat_1x8_i['d5 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd5] <= wr_dat_1x8_i['d6 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd6] <= wr_dat_1x8_i['d7 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[(adr_1x8_i << 'd3) + 'd7] <= wr_dat_1x8_i['d8 * DATA_WD-1 -: DATA_WD] ;
+      end
+      else begin  //col
+        mem_array[               adr_1x8_i] <= wr_dat_1x8_i[      DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d1 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d2 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d2 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d3 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d3 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d4 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d4 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d5 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d5 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d6 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d6 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d7 * DATA_WD-1 -: DATA_WD] ;
+        mem_array[('d7 << 'd3) + adr_1x8_i] <= wr_dat_1x8_i['d8 * DATA_WD-1 -: DATA_WD] ;
       end
     end
   end
 
   //rd
   always @(posedge clk or negedge rst_n) begin
-    if (rst_n) begin
+    if (!rst_n) begin
       rd_dat_1x1_r <= 'd0 ;
       rd_dat_1x8_r <= 'd0 ;
     end
-    else begin
-      if (rd_val_i_w) begin
-        if (rd_val_1x1_i) begin
-          rd_dat_1x1_r <= mem_array[adr_1x1_w] ;
-        end
-        else begin  //rd_val_1x8_i
-          if (dim_1x8_i) begin
-            rd_dat_1x8_r <= {
-              mem_array[(adr_1x8_i << 'd3) + 'd7] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd6] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd5] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd4] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd3] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd2] ,
-              mem_array[(adr_1x8_i << 'd3) + 'd1] ,
-              mem_array[(adr_1x8_i << 'd3)      ]
-            };
-          end
-          else begin
-            rd_dat_1x8_r <= {
-              mem_array[('d7 << 'd3) + adr_1x8_i] ,
-              mem_array[('d6 << 'd3) + adr_1x8_i] ,
-              mem_array[('d5 << 'd3) + adr_1x8_i] ,
-              mem_array[('d4 << 'd3) + adr_1x8_i] ,
-              mem_array[('d3 << 'd3) + adr_1x8_i] ,
-              mem_array[('d2 << 'd3) + adr_1x8_i] ,
-              mem_array[('d1 << 'd3) + adr_1x8_i] ,
-              mem_array[               adr_1x8_i]
-            };
-          end
-        end
+    else if (rd_vld_1x1_i) begin
+      rd_dat_1x1_r <= mem_array[adr_1x1_i] ;
+    end
+    else if (rd_vld_1x8_i)begin
+      if (!dim_sel_i) begin  //row
+        rd_dat_1x8_r <= {
+          mem_array[(adr_1x8_i << 'd3) + 'd7] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd6] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd5] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd4] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd3] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd2] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd1] ,
+          mem_array[(adr_1x8_i << 'd3) + 'd0]
+        };
+      end
+      else begin  //col
+        rd_dat_1x8_r <= {
+          mem_array[('d7 << 'd3) + adr_1x8_i] ,
+          mem_array[('d6 << 'd3) + adr_1x8_i] ,
+          mem_array[('d5 << 'd3) + adr_1x8_i] ,
+          mem_array[('d4 << 'd3) + adr_1x8_i] ,
+          mem_array[('d3 << 'd3) + adr_1x8_i] ,
+          mem_array[('d2 << 'd3) + adr_1x8_i] ,
+          mem_array[('d1 << 'd3) + adr_1x8_i] ,
+          mem_array[               adr_1x8_i]
+        };
       end
     end
   end
 
   always @(posedge clk or negedge rst_n) begin
-    if (rst_n) begin
-      rd_val_1x8_r <= 'd0 ;
-      rd_val_1x8_r <= 'd0 ;
+    if (!rst_n) begin
+      rd_vld_1x8_r <= 'd0 ;
+      rd_vld_1x8_r <= 'd0 ;
     end
     else begin
-      rd_val_1x1_r <= rd_val_1x1_i ;
-      rd_val_1x8_r <= rd_val_1x8_i ;
+      rd_vld_1x1_r <= rd_vld_1x1_i ;
+      rd_vld_1x8_r <= rd_vld_1x8_i ;
     end
   end
 
-  assign rd_val_1x1_o = rd_val_1x1_r ;
-  assign rd_val_1x8_o = rd_val_1x8_r ;
+  assign rd_vld_1x1_o = rd_vld_1x1_r ;
+  assign rd_vld_1x8_o = rd_vld_1x8_r ;
   assign rd_dat_1x1_o = rd_dat_1x1_r ;
   assign rd_dat_1x8_o = rd_dat_1x8_r ;
 
---
+++ sim/sim_fft_mem.v
@@ -0,0 +1,205 @@
+//------------------------------------------------------------------------------
+    //
+    //  Filename       : sim_fft_mem.v
+    //  Author         : LX
+    //  Created        : 2022-05-09
+    //  Description    : testbench for fft_mem
+    //                   
+//------------------------------------------------------------------------------
+
+//--- GLOBAL ---------------------------
+`define     SIM_TOP             sim_fft_mem
+`define     SIM_TOP_STR         "sim_fft_mem"
+`define     DUT_TOP             fft_mem
+`define     DUT_TOP_STR         "fft_mem"
+
+//--- LOCAL ----------------------------
+`define     DUT_FULL_CLK        10  // 100M
+`define     DUT_HALF_CLK        (`DUT_FULL_CLK / 2)
+
+//--- OTHER DEFINES --------------------
+// `define     DMP_SHM_FILE        "./simul_data/waveform.shm"
+// `define     DMP_FSDB_FILE       "./simul_data/waveform.fsdb"
+// `define     DMP_VCD_FILE        "./simul_data/waveform.vcd"
+// `define     DMP_EVCD_FILE       "./simul_data/waveform.evcd"
+
+
+module `SIM_TOP;
+//*** PARAMETER ****************************************************************
+  localparam FFT_DATA_WD     = 10 ;
+
+  localparam SIZE_MAT        = 8 ;
+  localparam SIZE_MAT_WD     = 3 ;
+  localparam SIZE_MAT_FUL    = SIZE_MAT    * SIZE_MAT    ;
+  localparam SIZE_MAT_FUL_WD = SIZE_MAT_WD + SIZE_MAT_WD ;
+
+//*** INPUT/OUTPUT *************************************************************
+reg                                      rst_n     ;
+reg                                      clk       ;
+reg                                      dim_sel   ;
+reg        [SIZE_MAT_WD           -1 :0] adr_1x8   ;
+reg                                      rd_vld_1x8;
+wire                                     rd_vld_1x8_o;
+wire       [SIZE_MAT*FFT_DATA_WD  -1 :0] rd_dat_1x8;
+reg                                      wr_vld_1x8;
+reg        [SIZE_MAT*FFT_DATA_WD  -1 :0] wr_dat_1x8;
+reg        [SIZE_MAT_FUL_WD       -1 :0] adr_1x1   ;
+reg                                      rd_vld_1x1;
+wire                                     rd_vld_1x1_o;
+wire       [FFT_DATA_WD           -1 :0] rd_dat_1x1;
+reg                                      wr_vld_1x1;
+reg        [FFT_DATA_WD           -1 :0] wr_dat_1x1;
+
+//*** WIRE/REG *****************************************************************
+wire       [FFT_DATA_WD           -1 :0] rd_data_unfold[0:7];
+
+integer i;
+
+
+//*** DUT **********************************************************************
+`DUT_TOP #(
+    .DATA_WD(FFT_DATA_WD)
+) dut (
+  .rst_n       ( rst_n        ),
+  .clk         ( clk          ),
+  .dim_sel_i   ( dim_sel      ),
+  .adr_1x8_i   ( adr_1x8      ),
+  .rd_vld_1x8_i( rd_vld_1x8   ),
+  .rd_vld_1x8_o( rd_vld_1x8_o ),
+  .rd_dat_1x8_o( rd_dat_1x8   ),
+  .wr_vld_1x8_i( wr_vld_1x8   ),
+  .wr_dat_1x8_i( wr_dat_1x8   ),
+  .adr_1x1_i   ( adr_1x1      ),
+  .rd_vld_1x1_i( rd_vld_1x1   ),
+  .rd_vld_1x1_o( rd_vld_1x1_o ),
+  .rd_dat_1x1_o( rd_dat_1x1   ),
+  .wr_vld_1x1_i( wr_vld_1x1   ),
+  .wr_dat_1x1_i( wr_dat_1x1   )
+);
+
+//*** MAIN BODY ****************************************************************
+  // clk
+  initial begin
+    clk = 'd0 ;
+    forever begin
+      #`DUT_HALF_CLK ;
+      clk = !clk ;
+    end
+  end
+
+  // rst_n
+  initial begin
+    rst_n = 'd0;
+    #(2 * `DUT_FULL_CLK);
+    rst_n = 'd1;
+  end
+
+  // rd_unfold
+  assign rd_data_unfold[0] = rd_dat_1x8[  FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[1] = rd_dat_1x8[2*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[2] = rd_dat_1x8[3*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[3] = rd_dat_1x8[4*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[4] = rd_dat_1x8[5*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[5] = rd_dat_1x8[6*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[6] = rd_dat_1x8[7*FFT_DATA_WD -1 -: FFT_DATA_WD];
+  assign rd_data_unfold[7] = rd_dat_1x8[8*FFT_DATA_WD -1 -: FFT_DATA_WD];
+
+  // main
+  initial begin
+    // log
+    $display( "\n\n*** CHECK %s BEGIN ! ***\n", `DUT_TOP_STR );
+
+    // init
+    dim_sel    = 0;
+    adr_1x8    = 0;
+    rd_vld_1x8 = 0;
+    wr_vld_1x8 = 0;
+    wr_dat_1x8 = 0;
+    adr_1x1    = 0;
+    rd_vld_1x1 = 0;
+    wr_vld_1x1 = 0;
+    wr_dat_1x1 = 0;
+
+    // delay
+    #(5 * `DUT_FULL_CLK);
+
+    // main
+    //---wr_1x1---
+    for (i=0; i<64; i=i+1) begin
+      @(posedge clk);
+      adr_1x1 = i;
+      wr_vld_1x1 = 1;
+      wr_dat_1x1 = i;
+    end
+    @(posedge clk);
+    wr_vld_1x1 = 0;
+
+    //---rd_1x1---
+    for (i=0; i<64; i=i+1) begin
+      @(posedge clk);
+      adr_1x1 = i;
+      rd_vld_1x1 = 1;
+    end
+    @(posedge clk);
+    rd_vld_1x1 = 0;
+
+    //---wr_1x8(row)---
+    for (i=0; i<8; i=i+1) begin
+      @(posedge clk);
+      adr_1x8 = i;
+      dim_sel = 0;
+      wr_vld_1x8 = 1;
+      wr_dat_1x8 = { 8{i[FFT_DATA_WD-1 : 0]} };
+    end
+    @(posedge clk);
+    wr_vld_1x8 = 0;
+
+    //---rd_1x8(row)---
+    for (i=0; i<8; i=i+1) begin
+      @(posedge clk);
+      adr_1x8 = i;
+      dim_sel = 0;
+      rd_vld_1x8 = 1;
+    end
+    @(posedge clk);
+    rd_vld_1x8 = 0;
+
+    //---wr_1x8(col)---
+    for (i=0; i<8; i=i+1) begin
+      @(posedge clk);
+      adr_1x8 = i;
+      dim_sel = 1;
+      wr_vld_1x8 = 1;
+      wr_dat_1x8 = { 8{i[FFT_DATA_WD-1 : 0]} };
+    end
+    @(posedge clk);
+    wr_vld_1x8 = 0;
+
+    //---rd_1x8(col)---
+    for (i=0; i<8; i=i+1) begin
+      @(posedge clk);
+      adr_1x8 = i;
+      dim_sel = 1;
+      rd_vld_1x8 = 1;
+    end
+    @(posedge clk);
+    rd_vld_1x8 = 0;
+  end
+
+  // finish
+  initial begin
+    #(1000*`DUT_FULL_CLK) $stop;
+  end
+
+
+//*** INIT *********************************************************************
+
+
+//*** CHKI *********************************************************************
+
+
+//*** CHKO *********************************************************************
+
+
+
+endmodule
