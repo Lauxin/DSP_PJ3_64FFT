9bc0ddb7d83d86b8983ad6b998f05e81ec10a087
TrevorLiu <976144243@qq.com>
2022-05-18 02:07:19 +0800

    [RTL & SIM] 1.add fft_wn, and check PASS! 2.rename fft_top to fft_ctrl, to avoid glue logic in top level.

---
+++ rtl/fft_wn.v
@@ -0,0 +1,110 @@
+//------------------------------------------------------------------------------
+  //
+  //  Filename       : fft_wn.v
+  //  Author         : LiuXun
+  //  Created        : 2022-05-12
+  //  Description    : 64 points FFT(DIT) wn
+  //
+//------------------------------------------------------------------------------
+
+module fft_wn(
+  fft_stg,
+  fft_idx,
+  fft_wn_re,
+  fft_wn_im
+);
+
+//*** PARAMETER ****************************************************************
+parameter FFT_WN_WD = 10;
+
+
+//*** INPUT/OUTPUT *************************************************************
+input         [1                 -1 :0] fft_stg;
+input         [3                 -1 :0] fft_idx;
+output reg    [7*FFT_WN_WD       -1 :0] fft_wn_re;
+output reg    [7*FFT_WN_WD       -1 :0] fft_wn_im;
+
+
+//*** WIRE/REG *****************************************************************
+wire          [FFT_WN_WD         -2 :0] mem_wn[0 : 16];
+
+
+//*** MAIN BODY ****************************************************************
+assign mem_wn[0 ] = 9'd0  ;
+assign mem_wn[1 ] = 9'd25 ;
+assign mem_wn[2 ] = 9'd50 ;
+assign mem_wn[3 ] = 9'd74 ;
+assign mem_wn[4 ] = 9'd98 ;
+assign mem_wn[5 ] = 9'd121;
+assign mem_wn[6 ] = 9'd142;
+assign mem_wn[7 ] = 9'd162;
+assign mem_wn[8 ] = 9'd181;
+assign mem_wn[9 ] = 9'd198;
+assign mem_wn[10] = 9'd213;
+assign mem_wn[11] = 9'd226;
+assign mem_wn[12] = 9'd237;
+assign mem_wn[13] = 9'd245;
+assign mem_wn[14] = 9'd251;
+assign mem_wn[15] = 9'd255;
+assign mem_wn[16] = 9'd256;
+
+//*** Four quadrant for the idx of Wn
+// 32~47(- +j) | 48~63(+ +j)
+//--------------------------
+// 16~31(- -j) | 0 ~15(+ -j)
+
+//*** Function of real and imag of Wn
+// re: mem_idx = |16-idx|
+// re: flag    = idx > 16
+// im: mem_idx = 16 - |16-idx|
+// im: flag    = idx > 0
+always @(*) begin
+  if (fft_stg == 1'd0) begin
+  //wn[0]
+  //wn[0::16]
+  //wn[0::8]
+    fft_wn_re = {
+      {1'b1,~mem_wn[8 ] + 1'b1},  //wn[24]
+      {1'b0, mem_wn[0 ]},  //wn[16]
+      {1'b0, mem_wn[8 ]},  //wn[8]
+      {1'b0, mem_wn[16]},  //wn[0]
+      {1'b0, mem_wn[0 ]},  //wn[16]
+      {1'b0, mem_wn[16]},  //wn[0]
+      {1'b0, mem_wn[16]}   //wn[0]
+    };
+    fft_wn_im ={
+      {1'b1,~mem_wn[8 ] + 1'b1},  //wn[24]
+      {1'b1,~mem_wn[16] + 1'b1},  //wn[16]
+      {1'b1,~mem_wn[8 ] + 1'b1},  //wn[8]
+      {1'b0, mem_wn[0 ]},  //wn[0]
+      {1'b1,~mem_wn[16] + 1'b1},  //wn[16]
+      {1'b0, mem_wn[0 ]},  //wn[0]
+      {1'b0, mem_wn[0 ]}   //wn[0]
+    };
+  end
+  else begin
+  //wn[i*4]
+  //wn[i*2::16]
+  //wn[i::8]
+    fft_wn_re = {
+      {1'b1                       , ~mem_wn[8  + fft_idx] + 1'b1},  //wn[i+24]
+      {(fft_idx > 0 ? 1'b1 : 1'b0), (mem_wn[fft_idx  ] ^ {9{fft_idx > 0}}) + (fft_idx > 0 ? 1'b1 : 1'b0)},  //wn[i+16]
+      {1'b0                       ,  mem_wn[8  - fft_idx]},  //wn[i+8]
+      {1'b0                       ,  mem_wn[16 - fft_idx]},  //wn[i]
+      {(fft_idx > 0 ? 1'b1 : 1'b0), (mem_wn[fft_idx*2] ^ {9{fft_idx > 0}}) + (fft_idx > 0 ? 1'b1 : 1'b0)},  //wn[2i+16]
+      {1'b0                       ,  mem_wn[16 - fft_idx*2]},  //wn[2i]
+      {(fft_idx > 4 ? 1'b1 : 1'b0), (mem_wn[fft_idx > 4 ? fft_idx*4 - 16 : 16 - fft_idx*4] ^ {9{fft_idx > 4}}) + (fft_idx > 4 ? 1'b1 : 1'b0)}   //wn[4i]
+    };
+    fft_wn_im = {
+      {1'b1                       , ~mem_wn[8  - fft_idx] + 1'b1},  //wn[i+24]
+      {1'b1                       , ~mem_wn[16 - fft_idx] + 1'b1},  //wn[i+16]
+      {1'b1                       , ~mem_wn[8  + fft_idx] + 1'b1},  //wn[i+8]
+      {(fft_idx > 0 ? 1'b1 : 1'b0), (mem_wn[fft_idx  ] ^ {9{fft_idx > 0}}) + (fft_idx > 0 ? 1'b1 : 1'b0)},  //wn[i]
+      {1'b1                       , ~mem_wn[16 - fft_idx*2] + 1'b1},  //wn[2i+16]
+      {(fft_idx > 0 ? 1'b1 : 1'b0), (mem_wn[fft_idx*2] ^ {9{fft_idx > 0}}) + (fft_idx > 0 ? 1'b1 : 1'b0)},  //wn[2i]
+      {(fft_idx > 0 ? 1'b1 : 1'b0), (mem_wn[fft_idx > 4 ? 32 - fft_idx*4 : fft_idx*4] ^ {9{fft_idx > 0}}) + (fft_idx > 0 ? 1'b1 : 1'b0)}   //wn[4i]
+    };
+  end
+end
+
+endmodule
---
+++ sim/sim_fft_wn.v
@@ -0,0 +1,172 @@
+//------------------------------------------------------------------------------
+    //
+    //  Filename       : sim_fft_wn.v
+    //  Author         : LX
+    //  Created        : 2022-05-15
+    //  Description    : testbench for fft_wn
+    //                   
+//------------------------------------------------------------------------------
+
+
+//--- GLOBAL ---------------------------
+`define     SIM_TOP             sim_fft_wn
+`define     SIM_TOP_STR         "sim_fft_wn"
+`define     DUT_TOP             fft_wn
+`define     DUT_TOP_STR         "fft_wn"
+
+//--- LOCAL ----------------------------
+`define     DUT_FULL_CLK        10  // 100M
+`define     DUT_HALF_CLK        (`DUT_FULL_CLK / 2)
+
+//--- OTHER DEFINES --------------------
+`define     CHKO_FFT_WN_FILE    "./check_data/fft8_wn_in.dat"
+
+// `define     DMP_SHM_FILE        "./simul_data/waveform.shm"
+// `define     DMP_FSDB_FILE       "./simul_data/waveform.fsdb"
+// `define     DMP_VCD_FILE        "./simul_data/waveform.vcd"
+// `define     DMP_EVCD_FILE       "./simul_data/waveform.evcd"
+
+
+module `SIM_TOP;
+//*** PARAMETER ****************************************************************
+  localparam FFT_WN_WD     = 10 ;
+
+
+//*** INPUT/OUTPUT *************************************************************
+  reg                                      fft_stg;
+  reg        [3                     -1 :0] fft_idx;
+  wire       [FFT_WN_WD*7           -1 :0] fft_wn_re;
+  wire       [FFT_WN_WD*7           -1 :0] fft_wn_im;
+
+
+//*** WIRE/REG *****************************************************************
+  wire       [FFT_WN_WD             -1 :0] wn_re_unfold[0:7-1];
+  wire       [FFT_WN_WD             -1 :0] wn_im_unfold[0:7-1];
+
+  reg                                      clk;
+  reg                                      rst_n;
+  reg                                      vld_in;
+
+  reg signed [FFT_WN_WD             -1 :0] ram_ref_wn_re[0:7-1];
+  reg signed [FFT_WN_WD             -1 :0] ram_ref_wn_im[0:7-1];
+
+  integer                                  wn_cnt;
+
+
+//*** DUT **********************************************************************
+  `DUT_TOP #(
+    .FFT_WN_WD(FFT_WN_WD)
+  ) dut (
+    .fft_stg   ( fft_stg   ),
+    .fft_idx   ( fft_idx   ),
+    .fft_wn_re ( fft_wn_re ),
+    .fft_wn_im ( fft_wn_im )
+  );
+
+
+//*** MAIN BODY ****************************************************************
+  // clk
+  initial begin
+    clk = 'd0 ;
+    forever begin
+      #`DUT_HALF_CLK ;
+      clk = !clk ;
+    end
+  end
+
+  // rst_n
+  initial begin
+    rst_n = 'd0;
+    #(2 * `DUT_FULL_CLK);
+    rst_n = 'd1;
+  end
+
+  // wn unfold
+  genvar i;
+  generate
+    for(i=0; i<7; i=i+1) begin:WN_UNFOLD
+      assign wn_re_unfold[i] = fft_wn_re[i*FFT_WN_WD +: FFT_WN_WD];
+      assign wn_im_unfold[i] = fft_wn_im[i*FFT_WN_WD +: FFT_WN_WD];
+    end
+  endgenerate
+
+  // main
+  initial begin
+    // log
+    $display( "\n\n*** CHECK %s BEGIN ! ***\n", `DUT_TOP_STR );
+
+    // init
+    vld_in = 0;
+    fft_stg = 0;
+    fft_idx = 0;
+
+    // delay
+    #(5 * `DUT_FULL_CLK);
+
+    // main
+    for (wn_cnt = 0; wn_cnt < 8; wn_cnt = wn_cnt + 1) begin
+      @(posedge clk);
+      vld_in = 1;
+      fft_stg = 0;
+      fft_idx = wn_cnt;
+    end
+    for (wn_cnt = 0; wn_cnt < 8; wn_cnt = wn_cnt + 1) begin
+      @(posedge clk);
+      vld_in = 1;
+      fft_stg = 1;
+      fft_idx = wn_cnt;
+    end
+    @(posedge clk);
+    vld_in = 0;
+  end
+
+  // finish
+  initial begin
+    #(100*`DUT_FULL_CLK) $stop;
+  end
+
+
+//*** INIT *********************************************************************
+
+
+//*** CHKI *********************************************************************
+
+
+//*** CHKO *********************************************************************
+  initial fork
+    CHKO_FFT_WN;
+  join
+
+  task CHKO_FFT_WN;
+    // variables
+    integer fp;
+    integer i;
+    integer tmp;
+    reg [7 -1 : 0] correct_flag;
+
+    // main body
+    begin
+      // open files
+      fp = $fopen(`CHKO_FFT_WN_FILE, "r");
+      // logs
+      $display("\t autocheck wn input...");
+      // core
+      forever begin
+        @(negedge clk);
+        if (vld_in) begin
+          for (i=0; i<7; i=i+1) begin
+            tmp = $fscanf(fp, "(%d+%dj), ", ram_ref_wn_re[i], ram_ref_wn_im[i]);
+            correct_flag[i] = (ram_ref_wn_re[i] == wn_re_unfold[i]) && (ram_ref_wn_im[i] == wn_im_unfold[i]);
+          end
+          if (&correct_flag) begin
+            $display("\t check PASS!");
+          end
+          else begin
+            $display("\t @(%t) check FAIL!", $time);
+          end
+        end
+      end
+    end
+  endtask
+
+endmodule
